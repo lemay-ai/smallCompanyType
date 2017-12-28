deba10184e443a7c8299220d6f52df7a7b63d98b
Daniel Shapiro <dcshapiro@gmail.com>
2017-12-27 21:56:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='smallCompanyType',
     version="1.0",
-    packages=find_packages(),
+    packages=['smallCompanyType'],
     include_package_data=True,
     install_requires=["gensim>=2.3.0","Keras>=2.1.2","sklearn","numpy>==1.13.3","h5py>=2.7.1"],
 )
