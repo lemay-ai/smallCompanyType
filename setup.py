77380f35b5553680cf303fa6b90dfb6439d8702a
Daniel Shapiro <dcshapiro@gmail.com>
2017-12-27 21:32:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["gensim","keras","sklearn","numpy","h5py"],
+    install_requires=["gensim>=2.3.0","Keras>=2.1.2","sklearn","numpy>==1.13.3","h5py>=2.7.1"],
 )
