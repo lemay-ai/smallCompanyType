393598834dfa9ff8eb742d06a825618c0f50362b
Daniel Shapiro <daniel@lemay.ai>
2019-01-04 15:06:12 +0000

    testing new method

--- test/test_pandas.py
+++ test/test_pandas.py
@@ -7,10 +7,11 @@ b=s.SmallCompanyType()
 files = glob.glob('TargetBusinesses_*.csv')
 
 def getCategory(row):
-    row["Category"]=b.getCompanyType(b.title_except(row["name"]))
+    row["Type"]=b.getCompanyType(b.title_except(row["name"]))
+    row["Subtype"]=b.getCompanySubtype(b.title_except(row["name"]))
     return row
 
 for file in files:
     df = pd.read_csv(file)
     df=df.apply(getCategory, axis=1)
-    print(df[['name','Category']])
+    print(df[['name','Type','Subtype']])
